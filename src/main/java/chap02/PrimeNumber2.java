9c5c960b4eb39ef49f3fa4b60c995e809a324d5e
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2022-01-20 13:43:49 +0900

    Finding prime numbers.

--- src/main/java/chap02/PrimeNumber2.java
+++ src/main/java/chap02/PrimeNumber2.java
@@ -1,21 +1,28 @@
 package chap02;
 
-public class PrimeNumber1 {
+public class PrimeNumber2 {
     public static void main(String[] args) {
         long beforeTime = System.currentTimeMillis();
         int counter = 0;
+        int ptr = 0;
+        int[] prime = new int[500];
 
-        for (int n = 2; n <= 1000; n++) {
+        prime[ptr++] = 2;
+
+        for (int n = 3; n <= 1000; n += 2) {
             int i;
-            for (i = 2; n <= 1000; n++) {
+            for (i = 1; i < ptr; i++) {
                 counter++;
-                if (n % i == 0) {
+                if (n % prime[i] == 0)
                     break;
-                }
             }
-            if (n == i) {
-                System.out.println(n);
+            if (ptr == i) {
+                prime[ptr++] = n;
             }
+        }
+
+        for (int i = 0; i < ptr; i++) {
+            System.out.println(prime[i]);
         }
 
         System.out.println("나눗셈을 수행한 횟수: " + counter);
